fc5574c2e261a08519039ff4b1b7990224dd1d18
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-03-26 17:03:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,185 @@
+import os
+import warnings
+import librosa
+import streamlit as st
+from joblib import dump, load
+from audio_analysis import audio_signals
+from audio_processing import extract_features
+
+st.header('Bird Species Classification')
+st.markdown('Sound of 114 Species of Birds :bird:')
+st.header('', divider='rainbow')
+
+uploaded_file = st.file_uploader("Choose a Audio file", accept_multiple_files=False)
+
+# Check if a file is uploaded
+if uploaded_file is not None:
+    # Get the path of the uploaded file
+    file_path = uploaded_file.name
+    st.write("Uploaded file path:", file_path)
+    if os.path.exists(file_path):
+        st.write("Uploaded file path:", file_path)
+    else:
+        st.write("Error: File not found.")
+
+audio_data, sampling_rate = librosa.load(file_path)
+st.audio(audio_data, sample_rate=sampling_rate)
+
+audio_signals(file_path)
+
+# Decorator for caching function results
+@st.cache_data
+def load_model(model_path):
+    return load(model_path)
+
+@st.cache_data
+def predict_emotion(audio_path, _model):
+    extracted_features = extract_features(audio_path).reshape(1, -1)
+    return _model.predict(extracted_features)
+
+
+# Load the model
+model_path = 'model.joblib'
+model = load_model(model_path)
+
+# Predict the emotion
+y_predict = predict_emotion(file_path, model)
+
+# Mapping for emotion labels
+labels_list = ['Fear', 'Angry', 'Neutral', 'Sad', 'Pleasant_Suprised', 'Disgust', 'Happy']
+encoded_label = [2, 0, 4, 6, 5, 1, 3]
+
+labels = {}
+for label, prediction in zip(encoded_label, labels_list):
+    labels[label] = prediction
+
+# Display predicted class
+if y_predict[0] in labels.keys():
+    st.subheader(f'Predicted Class: :rainbow[{labels[y_predict[0]]}]')
+
+
+
+
+# import os
+# import warnings
+# import librosa
+# import streamlit as st
+# from joblib import dump, load
+# from audio_analysis import audio_signals
+# from audio_processing import extract_features
+# # from audio_record import record_audio
+
+# st.header('Emotional Audio Classification', divider='rainbow')
+
+# emotion = st.selectbox(
+#     'Select a Emotion',
+#     ('Angry', 'Disgust', 'Fear',
+#        'Happy', 'Neutral', 'Pleasant_Suprised','Sad'))
+
+# st.write('You selected:', emotion)
+
+# dirs = os.listdir(f'raw_audio_data/{emotion}')
+
+# if emotion == 'Angry':
+
+#     file_list = []
+#     for dir_list in dirs:
+#     	files = ('raw_audio_data/'+emotion+'/'+dir_list)
+#     	file_list.append(files)
+        
+#     file_path = st.selectbox(
+#     'Select a Audio file',
+#     tuple(file_list))
+
+# elif emotion == 'Disgust':
+    
+#     file_list = []
+#     for dir_list in dirs:
+#     	files = ('raw_audio_data/'+emotion+'/'+dir_list)
+#     	file_list.append(files)
+        
+#     file_path = st.selectbox(
+#     'Select a Audio file',
+#     tuple(file_list))
+    
+# elif emotion == 'Fear':
+    
+#     file_list = []
+#     for dir_list in dirs:
+#     	files = ('raw_audio_data/'+emotion+'/'+dir_list)
+#     	file_list.append(files)
+        
+#     file_path = st.selectbox(
+#     'Select a Audio file',
+#     tuple(file_list))
+    
+# elif emotion == 'Happy':
+    
+#     file_list = []
+#     for dir_list in dirs:
+#     	files = ('raw_audio_data/'+emotion+'/'+dir_list)
+#     	file_list.append(files)
+        
+#     file_path = st.selectbox(
+#     'Select a Audio file',
+#     tuple(file_list))
+    
+# elif emotion == 'Neutral':
+    
+#     file_list = []
+#     for dir_list in dirs:
+#     	files = ('raw_audio_data/'+emotion+'/'+dir_list)
+#     	file_list.append(files)
+        
+#     file_path = st.selectbox(
+#     'Select a Audio file',
+#     tuple(file_list))
+    
+# elif emotion == 'Pleasant_Suprised':
+    
+#     file_list = []
+#     for dir_list in dirs:
+#     	files = ('raw_audio_data/'+emotion+'/'+dir_list)
+#     	file_list.append(files)
+        
+#     file_path = st.selectbox(
+#     'Select a Audio file',
+#     tuple(file_list))
+    
+# elif emotion == 'Sad':
+
+#     file_list = []
+#     for dir_list in dirs:
+#     	files = ('raw_audio_data/'+emotion+'/'+dir_list)
+#     	file_list.append(files)
+        
+#     file_path = st.selectbox(
+#     'Select a Audio file',
+#     tuple(file_list))
+
+# else:
+#     st.subheader('File not found')
+
+# audio_signals(file_path)
+
+
+# audio_data, sampling_rate = librosa.load(file_path)
+# audio_data = st.audio(audio_data, sample_rate=sampling_rate)
+
+# model_path = 'audio_classifier_model.joblib'
+# model = load(model_path)
+
+# audio = file_path
+# print(audio)
+
+# extracted_features = extract_features(audio).reshape(1, -1)
+# # extracted_features = x_test[112].reshape(1, -1)
+# y_predict = model.predict(extracted_features)
+# labels_list = ['Fear', 'Angry', 'Neutral', 'Sad', 'Pleasant_Suprised', 'Disgust', 'Happy']
+# encoded_label = [2, 0, 4, 6, 5, 1, 3]
+
+# labels = {}
+# for label, prediction in zip(encoded_label, labels_list):
+#     labels[label] = prediction
+# if y_predict[0] in labels.keys():
+#     st.subheader(f'Predicted Class: :rainbow[{labels[y_predict[0]]}]')
---
+++ audio_analysis.py
@@ -0,0 +1,79 @@
+import os
+import matplotlib.pyplot as plt
+plt.style.use('dark_background')
+import seaborn as sns
+import numpy as np
+import librosa
+from IPython.display import Audio
+import librosa
+import streamlit as st
+
+
+def audio_waveframe(file_path):
+    # Load the audio file
+    audio_data, sampling_rate = librosa.load(file_path)
+    # Calculate the duration of the audio file
+    duration = len(audio_data) / sampling_rate
+    # Create a time array for plotting
+    time = np.arange(0, duration, 1/sampling_rate)
+    # Plot the waveform
+    plt.figure(figsize=(30, 10))
+    plt.plot(time, audio_data, color='blue')
+    plt.title('Audio Waveform')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    # plt.savefig('audio_waveframe.png')
+    plot = st.pyplot(plt)
+
+    return plot
+
+
+# def spectrogram(file_path):
+#     # Compute the short-time Fourier transform (STFT)
+#     n_fft = 500  # Number of FFT points 2048
+#     hop_length = 1  # Hop length for STFT 512
+#     audio_data, sampling_rate = librosa.load(file_path)
+#     stft = librosa.stft(audio_data, n_fft=n_fft, hop_length=hop_length)
+#     # Convert the magnitude spectrogram to decibels (log scale)
+#     spectrogram = librosa.amplitude_to_db(np.abs(stft))
+#     # Plot the spectrogram
+#     plt.figure(figsize=(30, 10))
+#     # librosa.display.specshow(spectrogram, sr=sampling_rate, hop_length=hop_length, x_axis='time', y_axis='linear')
+#     librosa.display.specshow(spectrogram, sr=sampling_rate, hop_length=hop_length)
+#     plt.colorbar(format='%+2.0f dB')
+#     plt.title('Spectrogram')
+#     plt.xlabel('Time (s)')
+#     plt.ylabel('Frequency (Hz)')
+#     plt.tight_layout()
+#     # plt.savefig('spectrogram.png')
+#     plot = st.pyplot(plt)
+
+#     return plot
+
+
+def spectrogram(file_path):
+    
+    y, sr = librosa.load(file_path)
+    # Compute the spectrogram
+    D = librosa.stft(y)
+    # Convert magnitude spectrogram to decibels
+    DB = librosa.amplitude_to_db(abs(D))
+    # Plot the spectrogram
+    plt.figure(figsize=(10, 5))
+    librosa.display.specshow(DB, sr=sr, x_axis='time', y_axis='hz')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Spectrogram')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.tight_layout()
+    plot = st.pyplot(plt)
+
+    return plot
+
+
+
+def audio_signals(file_path):
+    aw = audio_waveframe(file_path)
+    spg = spectrogram(file_path)
+
+    return aw, spg
