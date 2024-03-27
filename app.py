9a53b33f71b3af0e7c8ca39ac0f9e737eb67a87a
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-03-27 19:27:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,19 @@ def predict_emotion(audio_path, _model):
     return _model.predict(extracted_features)
 
 audio_file = st.file_uploader("Upload an Audio file", type=["mp3", "wav", "ogg"], accept_multiple_files=False)
+# Load the model
+model_path = 'model.joblib'
+model = load_model(model_path)
+
+class_file = open('classes.json', 'r').read()
+labels_list = json.loads(class_file)
+
+st.header('', divider='rainbow')
+st.markdown('Download the Sample Audio here :point_down:')
+st.page_link("https://dibird.com/", label="DiBird.com", icon="🌎")
+st.subheader('Scientific Name of 114 Species of Birds :bird:')
+with st.container(height=300):
+    st.markdown(list(labels_list.values()))
 
 if audio_file is not None:
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
@@ -34,29 +47,13 @@ if audio_file is not None:
     audio_data, sampling_rate = librosa.load(file_path)
     st.audio(audio_data, sample_rate=sampling_rate)
     audio_signals(file_path)
-    
-    # Load the model
-    model_path = 'model.joblib'
-    model = load_model(model_path)
-    
-    class_file = open('classes.json', 'r').read()
-    labels_list = json.loads(class_file)
     # Predict the emotion
     y_predict = predict_emotion(file_path, model)
-    
     # Display predicted class
     if str(y_predict[0]) in labels_list.keys():
         st.subheader(f'Predicted Class: :rainbow[{labels_list[str(y_predict[0])][:-6]}]')
     else:
-        st.write('Class not Found')
-        
-    st.header('', divider='rainbow')
-    st.markdown('Download the Sample Audio here :point_down:')
-    st.page_link("https://dibird.com/", label="DiBird.com", icon="🌎")
-    st.subheader('Scientific Name of 114 Species of Birds :bird:')
-    with st.container(height=300):
-        st.markdown(list(labels_list.values()))
-            
+        st.write('Class not Found')      
 else:
     st.markdown('File not Found!')
     
