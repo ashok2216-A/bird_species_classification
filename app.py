388fd354c1055e5958826230ea326776b0b0f41f
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-03-27 11:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ st.header('', divider='rainbow')
 
 uploaded_file = st.file_uploader("Choose a Audio file", accept_multiple_files=False)
 audio_signals(uploaded_file)
+audio_data, sampling_rate = librosa.load(uploaded_file)
+st.audio(audio_data, sample_rate=sampling_rate)
+
 # # # Check if a file is uploaded
 # if uploaded_file is not None:
 #     # Get the path of the uploaded file
