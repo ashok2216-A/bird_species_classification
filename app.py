840efbd0636084c664238154db746c159b4fc6bf
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-03-27 11:34:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,37 +22,38 @@ if audio_file is not None:
     audio_signals(file_path)
     audio_data, sampling_rate = librosa.load(file_path)
     st.audio(audio_data, sample_rate=sampling_rate)
+
+    
+    
+    # Decorator for caching function results
+    @st.cache_data
+    def load_model(model_path):
+        return load(model_path)
+    
+    @st.cache_data
+    def predict_emotion(audio_path, _model):
+        extracted_features = extract_features(audio_path).reshape(1, -1)
+        return _model.predict(extracted_features)
+    
+    
+    # Load the model
+    model_path = 'model.joblib'
+    model = load_model(model_path)
+    
+    # Predict the emotion
+    y_predict = predict_emotion(file_path, model)
+    
+    # Mapping for emotion labels
+    labels_list = ['Fear', 'Angry', 'Neutral', 'Sad', 'Pleasant_Suprised', 'Disgust', 'Happy']
+    encoded_label = [2, 0, 4, 6, 5, 1, 3]
+    
+    labels = {}
+    for label, prediction in zip(encoded_label, labels_list):
+        labels[label] = prediction
+    
+    # Display predicted class
+    if y_predict[0] in labels.keys():
+        st.subheader(f'Predicted Class: :rainbow[{labels[y_predict[0]]}]')
+
 else:
     st.markdown('File not Found!')
-    
-
-# Decorator for caching function results
-@st.cache_data
-def load_model(model_path):
-    return load(model_path)
-
-@st.cache_data
-def predict_emotion(audio_path, _model):
-    extracted_features = extract_features(audio_path).reshape(1, -1)
-    return _model.predict(extracted_features)
-
-
-# Load the model
-model_path = 'model.joblib'
-model = load_model(model_path)
-
-# Predict the emotion
-y_predict = predict_emotion(file_path, model)
-
-# Mapping for emotion labels
-labels_list = ['Fear', 'Angry', 'Neutral', 'Sad', 'Pleasant_Suprised', 'Disgust', 'Happy']
-encoded_label = [2, 0, 4, 6, 5, 1, 3]
-
-labels = {}
-for label, prediction in zip(encoded_label, labels_list):
-    labels[label] = prediction
-
-# Display predicted class
-if y_predict[0] in labels.keys():
-    st.subheader(f'Predicted Class: :rainbow[{labels[y_predict[0]]}]')
-
