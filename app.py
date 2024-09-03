c37ff1888b9a537def8eaaa51f0c1952b2551408
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-09-03 18:11:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.header('', divider='rainbow')
 
 # Decorator for caching function results
 @st.cache_data
-def load_model(model_path):
+def model(model_path):
     return load_model(model_path)
 
 @st.cache_data
@@ -28,7 +28,7 @@ def predict_emotion(audio_path, _model):
 audio_file = st.file_uploader("Upload an Audio file", type=["mp3", "wav", "ogg"], accept_multiple_files=False)
 # Load the model
 model_path = 'bird_audio_classification_model.h5'
-model = load_model(model_path)
+model = model(model_path)
 
 class_file = open('classes.json', 'r').read()
 labels_list = json.loads(class_file)
