ac775eca2cfa907bd483a98b77e30a2c2c448d0a
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-11-05 11:19:46 +0530

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Medical Chatbot")
 st.title("Medical Chatbot 🤖")
 st.subheader("", divider='rainbow')
 # Get the Hugging Face token from environment variables
-hf_token = hf_token = st.secrets("HF_TOKEN")
+hf_token = st.secrets("HF_TOKEN")
 if hf_token is None:
     raise ValueError("Hugging Face token not found. Please set the HF_TOKEN environment variable.")
 
