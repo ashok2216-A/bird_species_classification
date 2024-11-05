66a52de5b0ac26d1b2c2b50649c7720fb874e462
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-11-05 10:21:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.set_page_config(
 )
 image = Image.open('logo.png')
 st.image(
-    image, width=100
+    image, width=50
 )
 # image = Image.open('logo.png')
 st.header('Bird Species Classification')
