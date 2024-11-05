51fbdf84f2addeeef8abd3afd2b0010c3ee0e3a0
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-11-05 12:03:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,8 +91,7 @@ if audio_file is not None:
 
         if user_input := f"Explain about {labels_list[str(y_predict)][:-6]} bird":
         # Generate and display assistant response   
-            response = LLM.respond(user_input, st.session_state.messages, max_tokens = 500, temperature = 0.70, top_p = 0.95)
-            st.markdown(response)
+            st.markdown(LLM.respond(user_input, st.session_state.messages, max_tokens = 500, temperature = 0.70, top_p = 0.95))
             # st.session_state.messages.append({"role": "assistant", "content": response})
         
     else:
